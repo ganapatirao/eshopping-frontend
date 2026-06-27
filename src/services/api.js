99fc1178b8814921a6f585579f822db80896d48a
ganeshtech <ganeshtech2017@gmail.com>
2026-06-27 19:53:05 +0530

    api settings

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://ganeshtech2017.runasp.net/api';
-const API_BASE_URL = 'http://localhost:5041/api';
+const API_BASE_URL = 'https://ganeshtech2017.runasp.net/api';
+// const API_BASE_URL = 'http://localhost:5041/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
